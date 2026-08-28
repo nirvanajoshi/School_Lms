39b0fb81119b945f24b7bb25561ddb46b6d0a118
nirvanajoshi <damodarj36@gmail.com>
2026-08-28 23:48:13 +0545

    urls and routing of config

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('announcements/', include('announcements.urls')),
+    path('assignments/', include('assignments.urls')),
+    path('attendance/', include('attendance.urls')),
+    path('courses/', include('courses.urls')),
 ]
